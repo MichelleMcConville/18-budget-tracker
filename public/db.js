b30723ad21740fbb36145d649e828142b0a09658
Michelle McConville <mchelmconvl@gmail.com>
2021-02-02 09:23:49 -0600

    added base checkDatabase

--- public/db.js
+++ public/db.js
@@ -29,3 +29,8 @@ function checkDatabase() {
   const transaction = db.transaction(["pending"], "readwrite");
   const store = transaction.objectStore("pending");
   const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    
+  };
+}
