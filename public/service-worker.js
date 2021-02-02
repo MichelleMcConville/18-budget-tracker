fff7cd786e039b385c81185e8eab8819a023f530
Michelle McConville <mchelmconvl@gmail.com>
2021-02-02 10:33:04 -0600

    added const FILES_TO_CACHE

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,14 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/index.js",
+  //   "/favicon.ico",
+  "/manifest.webmanifest",
+  "/styles.css",
+];
+
