2645fa7eafc8911ec8b88cf1b2cd73c0ebe22b48
Michelle McConville <mchelmconvl@gmail.com>
2021-02-02 09:21:32 -0600

    added saveRecord

--- public/db.js
+++ public/db.js
@@ -1,18 +1,25 @@
 let db;
 const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = function(event) {
- const db = event.target.result;
- db.createObjectStore("pending", { autoIncrement: true });
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
 };
 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
   db = event.target.result;
 
-  if (navigator.onLine) { checkDatabase(); }
+  if (navigator.onLine) {
+    checkDatabase();
+  }
 };
 
-request.onerror = function(event) {
+request.onerror = function (event) {
   console.log("Woops! " + event.target.errorCode);
 };
 
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
